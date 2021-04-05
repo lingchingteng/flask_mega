89c59cfb49d04582602204e3e55b0be878c1f289
CT_Ling <ctling0814@gmail.com>
2021-04-05 21:35:19 +0800

    Jinja2 for-loop comment

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,8 @@ from app import app
 @app.route("/index")
 def index():
     user = {"username": "CTLing"}
-    return render_template("index.html", title="Home", user=user)
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
