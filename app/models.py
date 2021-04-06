d514923ea163c7cb8605a52536b1151f3411b3a2
CT_Ling <ctling0814@gmail.com>
2021-04-06 22:11:20 +0800

    Preparing The User Model for Flask-Login

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
+from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
