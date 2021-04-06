990ea6f2c752273e0cfa9aec87df3c287ad511f5
CT_Ling <ctling0814@gmail.com>
2021-04-06 22:14:19 +0800

    User Loader Function

--- app/models.py
+++ app/models.py
@@ -3,7 +3,12 @@ from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from app import db
+from app import db, login
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class User(UserMixin, db.Model):
