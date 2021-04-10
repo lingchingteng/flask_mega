b903f174ddc7a9ed9ea4952ec3fec8f1f6a81f60
CT_Ling <ctling0814@gmail.com>
2021-04-10 11:47:59 +0800

    Submission of Blog Posts

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,8 @@ from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
 
 from app import app, db
-from app.forms import EditProfileForm, EmptyForm, LoginForm, RegistrationForm
-from app.models import User
+from app.forms import EditProfileForm, EmptyForm, LoginForm, PostForm, RegistrationForm
+from app.models import Post, User
 
 
 @app.before_request
@@ -16,15 +16,22 @@ def before_request():
         db.session.commit()
 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post is now live!")
+        return redirect(url_for("index"))
     posts = [
         {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
         {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
     ]
-    return render_template("index.html", title="Home", posts=posts)
+    return render_template("index.html", title="Home Page", form=form, posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
